b99aa9e2ec75914847ac098bb71c425e7429e57c
walia6 <awalia2002@gmail.com>
2018-02-08 21:13:28 -0600

    Added currency values to class.lua

--- class.lua
+++ class.lua
@@ -1,4 +1,5 @@
 classTypes={"terrain","farm","house","commons"}
+
 classData={
 	default = {
 		fill = config.colors.black,
@@ -8,22 +9,25 @@ classData={
 	terrain = {
 		fill = config.colors.green,
 		text = config.colors.white,
-		displayName = "Terrain"
+		displayName = "Terrain",
+		cost = -500
 	},
 	farm = {
 		fill = config.colors.paleyellow,
 		text = config.colors.darkgray,
-		displayName = "Farm"--,
-		--cost = 
+		displayName = "Farm",
+		cost = 750
 	},
 	house = {
 		fill = config.colors.brick,
 		text = config.colors.paleyellow,
-		displayName = "Home"
+		displayName = "Home",
+		cost = 1000
 	},
 	commons = {
 		fill = config.colors.purple,
 		text = config.colors.lightgray,
-		displayName = "Common Area"
+		displayName = "Common Area",
+		cost = 4500
 	}
 }
